d67e441d05ddd12512f2eb062a48cc5633f44cea
Shivansh Anand <shivanshanandisminame@outlook.com>
2020-08-17 21:40:32 +0530

    Add files via upload

---
+++ stack/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+  
+  
+  public static void main(String args[]) {
+    Stack st = new Stack(new Node("?"));
+    st.push(new Node("doing"));
+    st.push(new Node("you"));
+    st.push(new Node("How"));
+    st.display();
+  }
+}
