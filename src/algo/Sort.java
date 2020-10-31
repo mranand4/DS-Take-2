7cf0599d3c1a943c118e4b6aaeade56e541de84a
Shivansh Anand <shivanshanandisminame@outlook.com>
2020-10-31 09:31:44 +0530

    Added selection sort

--- src/algo/Sort.java
+++ src/algo/Sort.java
@@ -34,5 +34,32 @@ public class Sort {
 		
 		return arr;
 	}
+	
+	public int[] selectionSort() {
+		
+// * Selection sort algorithm.
+// * It searches for the smalllest value in each iteration and pushes that value in the proper ( starting from 0, in asc. ) index.
+// * Explanation : Suppose we have a list of numbers which we have to sort, say list1, how do we sort ? take another empty list, say list2,
+//   now, pick the smallest number in the first list and put in the second list ( and delete it from the first list of course )
+//   and repeat this operation for all the numbers, this is the essence of selection sort.
+// * This implementation is inplace i.e. it doesn't uses another list for sorting but concept is still thesame.
+// * Big O is O(n^2)
+		
+		int minIndex;
+		
+		for(int i=0; i<arr.length-1; i++) {
+			
+			minIndex = i;
+			
+			for(int j=i+1; j<arr.length; j++)
+				if(arr[minIndex] > arr[j])
+					minIndex = j;
+			
+			int temp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minindex] = temp;
+		}
+			    
+	}
 
 }
