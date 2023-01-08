833eedbc90cbf1341a08aec048e6383639e77440
Shivansh Anand <shivanshanandisminame@outlook.com>
2023-01-08 16:49:31 +0530

    Added more linked list functionalities

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -22,6 +22,36 @@ public class LinkedList {
         head = new Node(val);
     }
 
+    public int size() {
+        Node runner = head;
+        int size = 0;
+
+        while(runner != null) {
+            runner = runner.next;
+            size++;
+        }
+
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void show() {
+        Node runner = head;
+        StringBuilder sb = new StringBuilder();
+
+        while(runner != null) {
+            sb.append(runner.val);
+            sb.append("-->");
+            runner = runner.next;
+        }
+        sb.append(".");
+
+        System.out.println(sb);
+    }
+
     public void insertAtHead(int val) {
         Node node = new Node(val);
         node.next = head;
@@ -43,31 +73,74 @@ public class LinkedList {
             insertAtHead(val);
         }
 
-        Node node = new Node(val);
-        Node runner = head;
-        int currIdx = 0;
-
-        while(runner != null && currIdx < idx - 1) {
-            runner = runner.next;
-            currIdx++;
+        if(idx > size()) {
+            return;
         }
 
+        Node node = new Node(val);
+        Node runner = head;
+
+        /**
+         * Travelling till node@[idx - 1].
+         * After this loop, runner will point at node@[idx-1] i.e. node before node@[idx]
+         */
+        while(runner != null && --idx > 0) {
+            runner = runner.next;
+        }
+
+        /**
+         * Inserting node with value 'val' between node at index idx and node before it.
+         */
         node.next = runner.next;
         runner.next = node;
     }
 
-    public void show() {
+    public int valueAt(int idx) {
         Node runner = head;
-        StringBuilder sb = new StringBuilder();
 
-        while(runner != null) {
-            sb.append(runner.val);
-            sb.append("-->");
+        /**
+         * Travelling till node at index idx.
+         */
+        while(idx-- > 0) {
             runner = runner.next;
         }
-        sb.append(".");
 
-        System.out.println(sb);
+        return runner.val;
     }
 
+    public void removeHead() {
+        if(head != null) head = head.next;
+    }
+
+    public void removeTail() {
+        Node runner = head;
+
+        /**
+         * Travelling till node before tail.
+         */
+        while(runner.next.next != null) {
+            runner = runner.next;
+        }
+
+        runner.next = null;
+    }
+
+    public void removeAt(int idx) {
+        if(idx < size()) {
+            if (idx == 0) {
+                removeHead();
+            } else {
+                Node runner = head;
+                /**
+                 * Travelling to node at index [idx-1] i.e. node before the node which is going to be deleted.
+                 */
+                while (runner != null && --idx > 0) {
+                    runner = runner.next;
+                }
+
+                runner.next = runner.next.next;
+            }
+        }
+
+    }
 }
